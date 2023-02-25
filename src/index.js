921d1b25142b2f1f399267549f8891279a3c1049
fingla <felixinglavf@gmail.com>
2023-02-25 14:59:56 +0100

    Update and fix index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
-import { createRoot } from 'react-dom/client';
-import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
+import ReactDOM from 'react-dom';
+import Konva from 'konva';
+import { Stage, Layer, Rect, Text } from 'react-konva';
 
-var randomColor = Math.floor(Math.random()*16777215).toString(16);
+const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
 
 const App = () => {
+  const handleRefresh = () => {
+    window.location.reload();
+  }
+
   return (
     <Stage width={window.innerWidth} height={window.innerHeight}>
       <Layer>
         <Text text="Refresh to change color" fontSize={15} />
         <Rect
-          x={20}
+          x={50}
           y={50}
           width={100}
-          height={100}
+          height={50}
           fill={randomColor}
+          stroke={'black'}
+          strokeWidth={5}
+          onClick={handleRefresh}
         />
       </Layer>
     </Stage>
+    
   );
 };
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
